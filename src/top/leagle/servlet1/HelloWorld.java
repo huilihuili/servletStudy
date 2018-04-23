f51526207c3d14b82815d1c6028ce46ed9a2a061
huilihuili <864814732@qq.com>
2018-04-23 15:12:45 +0800

    Hello, world!

---
+++ src/top/leagle/servlet1/HelloWorld.java
@@ -0,0 +1,37 @@
+package top.leagle.servlet1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private String message;
+
+	@Override
+	public void init() throws ServletException {
+		// 执行必需的初始化
+		message = "Hello, world!";
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello, world!");
+		// 设置响应内容类型
+		response.setContentType("text/html");
+
+		// 实际的逻辑是在这里
+		PrintWriter out = response.getWriter();
+		out.println("<h1>" + message + "</h1>");
+	}
+
+	@Override
+	public void destroy() {
+		// 什么也不做
+	}
+}
